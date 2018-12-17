0fd3b7d704e8626b2812cd035b95b947abcfb43c
Mykyta Pashura <rowdyswa@gmail.com>
2018-12-17 21:01:56 +0200

    Fix unit tests

--- utils/id_generator/id_generator_test.go
+++ utils/id_generator/id_generator_test.go
@@ -13,27 +13,33 @@ func TestIdGenerator_Generate(t *testing.T) {
 		ids = append(ids, id)
 	}
 
-	if len(ids) != 10 {
-		t.Errorf("got len of ids: %v, wanted: %v", len(ids), 10)
+	expectedLenOfIDs := 10
+	if len(ids) != expectedLenOfIDs {
+		t.Errorf("got len of ids: %v, wanted: %v", len(ids), expectedLenOfIDs)
 	}
 
-	if ids[4] != 5 {
-		t.Errorf("got 4th id equals to: %v, wanted: %v", ids[4], 10)
+	expectedForthID := 5
+	if ids[4] != expectedForthID {
+		t.Errorf("got 4th id equals to: %v, wanted: %v", ids[4], expectedForthID)
 	}
 
 }
 
 func TestIdGenerator_Current(t *testing.T) {
+	Generator.counter = 0
+
 	currentId := Generator.Current()
 
-	if currentId != 0 {
-		t.Errorf("got current id equals to: %v, wanted: %v", currentId, 0)
+	expectedCurrentID := 0
+	if currentId != expectedCurrentID {
+		t.Errorf("got current id equals to: %v, wanted: %v", currentId, expectedCurrentID)
 	}
 
 	Generator.Generate()
 	currentIdAfterGenerate := Generator.Current()
 
-	if currentIdAfterGenerate != 1 {
-		t.Errorf("got current id equals to: %v, wanted: %v", currentIdAfterGenerate, 1)
+	expectedCurrentIDAfterGenerate := 1
+	if currentIdAfterGenerate != expectedCurrentIDAfterGenerate {
+		t.Errorf("got current id equals to: %v, wanted: %v", currentIdAfterGenerate, expectedCurrentIDAfterGenerate)
 	}
 }
